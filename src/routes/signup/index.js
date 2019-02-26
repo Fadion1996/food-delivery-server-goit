89ebae23d2d3a38dcdebf8cf0914758a173ef819
Vasiliy Yurkovets <vyurkovets@int.codinsula.com>
2019-02-26 14:52:59 +0200

    Done homework #1

--- src/routes/signup/index.js
+++ src/routes/signup/index.js
@@ -1,9 +1,12 @@
 const qs = require('querystring');
-const saveUser = user => {
-    debugger
+const fs = require("fs");
+let path = '';
 
-    
-    // res.send(data);
+const saveUser = user => {
+  path = `./src/db/users/${user.user.username}.txt`;
+  fs.writeFile(`./src/db/users/${user.user.username}.txt`, JSON.stringify(user), ()=> {
+      console.log(`Write file: ${user.user.username}.txt`);
+  });
   // получить файл с юзером
   // найти путь папки users
   // сохранить туда файл
@@ -14,31 +17,36 @@ const signupRoute = (request, response) => {
 
   if (request.method === 'POST') {
     let body = '';
-    debugger
-
     request.on('data', data => {
-        body = body + data;
-        debugger
-
-        const user = {
-            username: user.body.username,
-            telephone: user.body.telephone,
-            password:  user.body.password,
-            email: user.body.email,
-            success: true
-        };
-
-        console.log(user);
-
-      console.log('Incoming data!!!!');
+      body = body + data;
+      const parseBody = JSON.parse(body);
+      
+      const result = {
+        status: 'success',
+        user: {
+          username: parseBody.username,
+          telephone: parseBody.telephone,
+          password:  parseBody.password,
+          email: parseBody.email,
+        }
+      };
+      responseUser(result);
+      saveUser(result);
     });
 
     request.on('end', () => {
       const post = qs.parse(body);
-      console.log(post);
     });
   }
 
+  responseUser = (userData) => {
+    const userName = userData.user.username;
+    fs.writeFile(`./src/db/users/${userName}.json`, JSON.stringify(userData), (err, data) => {
+      response.writeHead(200, {"Content-Type": "application/json"});
+      response.end(JSON.stringify(userData));
+    })
+  }
+  
   // Взять username с данных, сохранить в переменную
 
   // Сохраняем данные в <username>.json
@@ -47,6 +55,7 @@ const signupRoute = (request, response) => {
 
   // Отправляем файл в ответе с данными юзера
   // использовать response
+    
 };
 
 module.exports = signupRoute;
